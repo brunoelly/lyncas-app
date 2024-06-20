09600c68d99a22b1a079c2ff1b69abfd64155850
brunoelly <brunoelly@outlook.com>
2024-06-20 15:05:12 -0300

    Keycloak improvement

---
+++ .history/src/main/resources/db/migration/V1__create_payable_accounts_20240620141536.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS PAYABLE_ACCOUNTS.payable_accounts (
+    id SERIAL PRIMARY KEY,
+    due_date DATE NOT NULL,
+    payment_date DATE,
+    amount DECIMAL(10, 2) NOT NULL,
+    account_description VARCHAR(255),
+    payment_status VARCHAR(50)
+);
